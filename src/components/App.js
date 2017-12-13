b5ff063cc3e6d418c0766d2f4bf6666c123d2c94
Jillian Somera <jsomera@gmail.com>
2017-12-13 11:08:03 -0600

    Remove class names

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <header>
           <h1>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
